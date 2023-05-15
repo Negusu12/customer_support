5c83345a824fe591e76727c92ac7e963191e8436
Negusu12 <negusu00@yahoo.com>
2023-05-15 12:22:46 +0300

    added a notification reminder

--- index.php
+++ index.php
@@ -1,11 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php include('db_connect.php') ?>
 <?php session_start() ?>
 <?php
 if (!isset($_SESSION['login_id']))
   header('location:login.php');
 include 'header.php'
 ?>
+<style>
+  img {
+    height: 50px;
+    width: 50px;
+    background-color: red;
+    margin-top: -7px;
+    margin-left: 80%;
+    animation: shake 0.5s ease-in-out infinite;
+  }
+
+  @keyframes shake {
+    0% {
+      transform: translateX(0);
+    }
+
+    10%,
+    30%,
+    50%,
+    70%,
+    90% {
+      transform: translateX(-5px);
+    }
+
+    20%,
+    40%,
+    60%,
+    80% {
+      transform: translateX(5px);
+    }
+
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  .notification-container {
+    position: relative;
+    display: inline-block;
+    margin-left: 80%;
+  }
+
+  .notification-count {
+    position: absolute;
+    top: -10px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: red;
+    color: white;
+    font-size: 12px;
+    text-align: center;
+    line-height: 20px;
+
+  }
+</style>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
   <div class="wrapper">
@@ -23,8 +81,21 @@ include 'header.php'
       <div class="content-header">
         <div class="container-fluid">
           <div class="row mb-2">
-            <div class="col-sm-6">
+            <div class="col-sm-6 bell row">
               <h1 class="m-0"><?php echo $title ?></h1>
+
+              <?php if ($_SESSION['login_type'] == 2) : ?>
+
+                <?php $ticket_count = $conn->query("SELECT * FROM tickets WHERE (tickets.status = 0 OR tickets.status = 1)")->num_rows; ?>
+                <?php if ($ticket_count > 0) : ?>
+                  <div class="notification-container">
+                    <img src="assets/dist/img/bell-solid.png">
+                    <span class="notification-count"><?php echo $ticket_count; ?></span>
+                  </div>
+                <?php endif; ?>
+
+              <?php endif; ?>
+
             </div><!-- /.col -->
 
           </div><!-- /.row -->
